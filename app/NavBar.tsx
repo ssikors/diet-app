d46a6c517416b990b949f7a59b6b3c19040b1b42
ssikors <stanislaw.s.2000@gmail.com>
2024-02-28 15:38:22 +0100

    navbar changes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from "next/link"
 import { FaBowlFood } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const path = usePathname()
+
   const links = [
     { label: "Meals", href: "/meals"},
     { label: "My meals", href: "/my-meals"}
@@ -11,7 +16,7 @@ const NavBar = () => {
     <nav className="flex text-xl space-x-6 border-b mb-6 px-5 h-14 items-center">
       <Link href={""} className="text-2xl"><FaBowlFood/></Link>
       <ul className="flex space-x-6">
-        {links.map((link) => <Link key={link.label} className="text-slate-600 hover:text-zinc-900" href={link.href}>{link.label}</Link>)}
+        {links.map((link) => <Link key={link.label} className={`${link.href == path ? "text-slate-800" : "text-slate-600" } hover:text-slate-900`} href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
   )
