09ea350265eb8b9f344956acb7180544eb874411
ssikors <stanislaw.s.2000@gmail.com>
2024-03-07 17:56:32 +0100

    added hero page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar = () => {
 
   return (
     <nav className="flex text-xl space-x-6 border-b mb-6 px-5 h-14 items-center">
-      <Link href={""} className="text-2xl"><FaBowlFood/></Link>
+      <Link href={"/"} className="text-2xl"><FaBowlFood/></Link>
       <ul className="flex space-x-6">
         {links.map((link) => <Link key={link.label} className={`${link.href == path ? "text-slate-800" : "text-slate-600" } hover:text-slate-900`} href={link.href}>{link.label}</Link>)}
       </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+import Link from "next/link";
+import { Button } from "./components/basic/Button";
 
 export default function Home() {
   return (
-    <div>Null</div>
+    <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Meals suited for your diet
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Our app makes it easy for you to find the meals you need.
+          Browse through recipes posted by other users or share your own ideas!
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link href={"/meals"}>
+            <Button>Get started</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
